aa905b1a2d09e26c978a6f1815c7f26b9ac84fe8
TheDingusThing <the.dingus.thing@outlook.com>
2023-02-11 12:20:54 +0200

    Loading module now uses current working directory

--- models/experimental.py
+++ models/experimental.py
@@ -1,3 +1,5 @@
+import os
+import sys
 import numpy as np
 import random
 import torch
@@ -248,6 +250,7 @@ def attempt_load(weights, map_location=None):
     # Loads an ensemble of models weights=[a,b,c] or a single model weights=[a] or weights=a
     model = Ensemble()
     for w in weights if isinstance(weights, list) else [weights]:
+        w = f"{os.getcwd()}/{w}"
         attempt_download(w)
         ckpt = torch.load(w, map_location=map_location)  # load
         model.append(ckpt['ema' if ckpt.get('ema') else 'model'].float().fuse().eval())  # FP32 model
